d07f6a39f99f2cfad85d20b6f1c909da2cc03821
Edmund Wong <edmundpunch5@gmail.com>
2025-11-04 16:03:25 -0500

    Created modals

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -3,8 +3,11 @@
 import { Plus } from "lucide-react"
 
 import { ActionTooltip } from "@/components/action-tooltip"
+import { useModal } from "@/hooks/use-modal-store"
 
 export const NavigationAction = () => {
+  const { onOpen } = useModal();
+
   return (
     <div>
       <ActionTooltip
@@ -13,6 +16,7 @@ export const NavigationAction = () => {
         label="Add a server"
       >
         <button
+          onClick={() => onOpen("createServer")}  // Opens "create-server-modal.tsx" We wrote in "create-server-modal.tsx" to open it if the modal type is "createServer"
           className="group cursor-pointer flex items-center"
         >
           <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 dark:group-hover:bg-emerald-500 group-hover:bg-emerald-500">
